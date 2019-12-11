e6440ba2977ab3ed1db2dd6ad2d189cebeb4d624
jeremy.zhang <zhangyue9306@gmail.com>
2019-12-11 16:55:12 +0800

    update server

--- greet/greetserver/server.go
+++ greet/greetserver/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/grpc-go-course/greet/greetpb"
 	"google.golang.org/grpc"
@@ -12,11 +13,20 @@ type server struct {
 
 }
 
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	result := "Hello " + req.Greeting.FirstName + req.Greeting.LastName
+
+	response := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return response, nil
+}
+
 func main() {
 
 	fmt.Println("Inside server ...")
 
-	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")  // open a port
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
